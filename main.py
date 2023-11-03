0e19d576b59305a2b874de3a9002b6e0fe1a0e7a
Alhassan Ramadan <67296782+Matarwy@users.noreply.github.com>
2023-11-03 17:03:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from PIL import Image
 import pytesseract
 from os import path
+from docx import Document
+
+# Create a new Word document
+doc = Document()
 for i in range(1, 31):
     filepath = path.relpath(f'img/{i}.jpg')
+    # Open the image
     image = Image.open(filepath)
+
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    # Use pytesseract to extract text from the image
     text = pytesseract.image_to_string(image)
-    with open(f'txt/{i}.txt', 'w') as file:
-        file.write(text)
+
+    # Add the extracted text to the Word document
+    doc.add_paragraph(text)
+
+# Save the Word document
+doc.save('output.docx')
 
